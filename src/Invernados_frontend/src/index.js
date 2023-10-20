d3e6a0c30a7ff2d79dd4ddb688ee84b2851fcf8a
Crowdake <jaime_jesabel@hotmail.com>
2023-10-20 01:35:04 -0600

    hola

--- src/Invernados_frontend/src/index.js
+++ src/Invernados_frontend/src/index.js
@@ -3,17 +3,19 @@ import { Invernados_backend } from "../../declarations/Invernados_backend";
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
   const button = e.target.querySelector("button");
-
   const name = document.getElementById("name").value.toString();
+  const a = parseInt(document.getElementById("a").value);
+  const b = parseInt(document.getElementById("b").value);
 
   button.setAttribute("disabled", true);
 
   // Interact with foo actor, calling the greet method
   const greeting = await Invernados_backend.greet(name);
+  const result = await Invernados_backend.add(a, b);
 
   button.removeAttribute("disabled");
-
   document.getElementById("greeting").innerText = greeting;
+  document.getElementById("number").innerText = result.toString();
 
   return false;
 });
